794a07eaeb0fac32cfafc500aca841fc648a4ed3
Kyamel <lcamelo200@gmail.com>
2026-01-31 23:48:18 -0300

    refractor: split scripts

--- script.js
+++ script.js
@@ -14,29 +14,6 @@ function processSHA256() {
   document.getElementById("sha256Output").value = hash;
 }
 
-// Função de cifra ROT
-function rotCipher(text, rotation) {
-  let result = "";
-  for (const char of text) {
-    if (char >= 'a' && char <= 'z') {
-      const base = 'a'.charCodeAt(0);
-      const newChar = (char.charCodeAt(0) - base + rotation) % 26 + base;
-      result += String.fromCharCode(newChar);
-    } else if (char >= 'A' && char <= 'Z') {
-      const base = 'A'.charCodeAt(0);
-      const newChar = (char.charCodeAt(0) - base + rotation) % 26 + base;
-      result += String.fromCharCode(newChar);
-    } else if (char >= '0' && char <= '9') {
-      const base = '0'.charCodeAt(0);
-      const newChar = (char.charCodeAt(0) - base + rotation) % 10 + base;
-      result += String.fromCharCode(newChar);
-    } else {
-      result += char;
-    }
-  }
-  return result;
-}
-
 // Atualizar rotação
 function updateRotation() {
   currentRotation = parseInt(document.getElementById("rotation").value);
